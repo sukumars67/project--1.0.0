dda27ac6ad56e1aa5cba650559e4da5ec8671df6
SUKUMARS <sukumars6644@gmail.com>
2025-02-08 11:26:57 +0530

    First react app

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Hello Sukumar
+      </div>
+    </>
+  )
+}
+
+export default App
